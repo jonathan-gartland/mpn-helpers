6086a2e04bd19bfb40abf4c8884344977a63bd70
jonathan gartland <jonny@hal.local>
2022-12-20 13:50:54 -0500

    added package

--- tests/test_qtmpn.py
+++ tests/test_qtmpn.py
@@ -1,6 +1,7 @@
 from src import QTArrays
 # import pytest
 
+
 def test_qt51():
     # 18 22.2 14.1 35.2
     assert QTArrays.getQTmpn(18) == [22.2,14.1,35.2]
@@ -11,9 +12,9 @@ def test_qt51():
     # 30 45.3 31.5 65.6
     assert QTArrays.getQTmpn(30) == [45.3,31.5,65.6]
 
-    assert QTArrays.getQTmpn(-1) == None
+    assert QTArrays.getQTmpn(-1) is None
 
-    assert QTArrays.getQTmpn('b') == None
+    assert QTArrays.getQTmpn('b') is None
 
 
 # @pytest.mark.xfail(raises=AssertionError)
@@ -23,24 +24,21 @@ def test_qt51():
 #         print(bad_result)
 #     except Exception as e:
 #         print(e)
-    
 #     pass
 
 def test_qt2k():
     # [59.3, 44.6, 77.2]
-    assert QTArrays.getQT2Kmpn(22,22) == [59.3, 44.6, 77.2]
+    assert QTArrays.getQT2Kmpn(22, 22) == [59.3, 44.6, 77.2]
     # ['&lt;1', 0.0, 3.7]
-    assert QTArrays.getQT2Kmpn(0,0) == ['<1', 0.0, 3.7]
+    assert QTArrays.getQT2Kmpn(0, 0) == ['<1', 0.0, 3.7]
     # ['&gt;2419.6', 1439.5, 'infinite']
-    assert QTArrays.getQT2Kmpn(49,48) == ['>2419.6', 1439.5, 'infinite']
+    assert QTArrays.getQT2Kmpn(49, 48) == ['>2419.6', 1439.5, 'infinite']
 
-    assert QTArrays.getQT2Kmpn(-2,-10) == None
+    assert QTArrays.getQT2Kmpn(-2, -10) is None
 
     # assert QTArrays.getQT2Kmpn('q','a') == None
 
-    
-
 
 def test_legio():
-    assert QTArrays.getQTLEGIOmpn(0,0) == '<1'
+    assert QTArrays.getQTLEGIOmpn(0, 0) == '<1'
     pass
